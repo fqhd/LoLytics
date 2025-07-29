13e26e40158ab9064589ff99fd9886909f125caa
Fahd <fahd31256@gmail.com>
2025-07-29 14:40:55 +0100

    refactor(model): encapsulate database creation

--- model/create_lmdb_dataset.py
+++ model/create_lmdb_dataset.py
@@ -6,14 +6,10 @@ from tqdm import tqdm
 import os
 import random
 
-with open('champion_to_index.json') as f:
-    champ_to_idx = json.load(f)
+def convert_json_sample_to_numpy(sample, champ_to_idx):
+    arr = []
 
-dragon_names = ['WATER_DRAGON', 'AIR_DRAGON', 'CHEMTECH_DRAGON', 'FIRE_DRAGON', 'HEXTECH_DRAGON', 'EARTH_DRAGON']
-
-def convert_json_sample_to_numpy(sample):
-    l = []
-
+    dragon_names = ['WATER_DRAGON', 'AIR_DRAGON', 'CHEMTECH_DRAGON', 'FIRE_DRAGON', 'HEXTECH_DRAGON', 'EARTH_DRAGON']
     player_keys = [
         'champion', 'kills', 'deaths', 'assists', 'baronTimer', 'elderTimer',
         'deathTimer', 'gold', 'level', 'creepscore', 'x', 'y'
@@ -23,27 +19,27 @@ def convert_json_sample_to_numpy(sample):
         for player in team['players']:
             for k in player_keys:
                 if k == 'champion':
-                    l.append(champ_to_idx[player['champion']])
+                    arr.append(champ_to_idx[player['champion']])
                 elif k == 'deathTimer':
-                    l.append(round(player['deathTimer']))
+                    arr.append(round(player['deathTimer']))
                 else:
-                    l.append(player[k])
+                    arr.append(player[k])
 
         for i in range(4):
             one_hot = [0, 0, 0, 0, 0, 0]
             if i < len(team['drakes']):
                 name = team['drakes'][i]
                 one_hot[dragon_names.index(name)] = 1
-            l += one_hot
-        l.append(team['rifts'])
-        l.append(team['atakhan'])
-        l.append(team['grubs'])
-        l += team['towers']
-        l += team['inhibs']
-    l.append(sample['time'])
-    l.append(sample['win'])
+            arr += one_hot
+        arr.append(team['rifts'])
+        arr.append(team['atakhan'])
+        arr.append(team['grubs'])
+        arr += team['towers']
+        arr += team['inhibs']
+    arr.append(sample['time'])
+    arr.append(sample['win'])
 
-    return np.array(l, dtype='int32')
+    return np.array(arr, dtype='int32')
 
 def get_game_paths(data_dir):
     paths = []
@@ -62,6 +58,9 @@ def save_lmdb(paths, lmdb_path):
     map_size = 1 << 40
     env = lmdb.open(lmdb_path, map_size=map_size)
 
+    with open('champion_to_index.json') as f:
+        champ_to_idx = json.load(f)
+
     idx = 0
     with env.begin(write=True) as txn:
         for path in tqdm(paths):
@@ -69,7 +68,7 @@ def save_lmdb(paths, lmdb_path):
             file = random.choice(game_snapshot_files)
             with open(os.path.join(path, file)) as f:
                 obj = json.load(f)
-                data = convert_json_sample_to_numpy(obj)
+                data = convert_json_sample_to_numpy(obj, champ_to_idx)
             key = str(idx).encode()
             value = pickle.dumps(data)
             txn.put(key, value)
