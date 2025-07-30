12c821dba10808cb7ad4b40b2440f5404487f221
Fahd <fahd31256@gmail.com>
2025-07-30 16:08:47 +0100

    refactor(game): split up initial state creation

--- data/game.js
+++ data/game.js
@@ -16,59 +16,63 @@ export async function get_game_data(MATCH_ID, key) {
 	return states;
 }
 
+function create_players() {
+	const players = [];
+	for (let j = 0; j < 5; j++) {
+		const playerIndex = i * 5 + j;
+		const champion = game.info.participants[playerIndex].championName;
+		players.push({
+			champion,
+			kills: 0,
+			deaths: 0,
+			assists: 0,
+			baronTimer: 0,
+			elderTimer: 0,
+			deathTimer: 0,
+			gold: 0,
+			level: 1,
+			creepscore: 0,
+			x: 0,
+			y: 0,
+		});
+	}
+	return players;
+}
+
+function create_team() {
+	return {
+		players: create_players(),
+		drakes: [],
+		rifts: 0,
+		atakhan: 0,
+		grubs: 0,
+		towers: [
+			1, // Top outer
+			1, // Top inner
+			1, // Top base
+			1, // Mid outer
+			1, // Mid inner
+			1, // Mid base
+			1, // Bot outer
+			1, // Bot inner
+			1, // Bot base
+			0, // Nexus
+			0, // Nexus
+		],
+		inhibs: [
+			0, // Top
+			0, // Mid
+			0, // Bot
+		],
+	};
+}
+
 function create_initial_state(game) {
-	const state = {
-		teams: [],
+	return {
+		teams: [create_team(), create_team()],
 		time: -1,
 		win: game.info.participants[0].win,
 	};
-	for (let i = 0; i < 2; i++) {
-		const team = {
-			players: [],
-			drakes: [],
-			rifts: 0,
-			atakhan: 0,
-			grubs: 0,
-			towers: [
-				1, // Top outer
-				1, // Top inner
-				1, // Top base
-				1, // Mid outer
-				1, // Mid inner
-				1, // Mid base
-				1, // Bot outer
-				1, // Bot inner
-				1, // Bot base
-				0, // Nexus
-				0, // Nexus
-			],
-			inhibs: [
-				0, // Top
-				0, // Mid
-				0, // Bot
-			],
-		};
-		for (let j = 0; j < 5; j++) {
-			const playerIndex = i * 5 + j;
-			const champion = game.info.participants[playerIndex].championName;
-			team.players.push({
-				champion,
-				kills: 0,
-				deaths: 0,
-				assists: 0,
-				baronTimer: 0,
-				elderTimer: 0,
-				deathTimer: 0,
-				gold: 0,
-				level: 1,
-				creepscore: 0,
-				x: 0,
-				y: 0,
-			});
-		}
-		state.teams.push(team);
-	}
-	return state;
 }
 
 function update_with_frame(state, frame) {
