18430ef2d98c2b5ca03c9148dae3033320d4fca3
Fahd <fahd31256@gmail.com>
2025-07-30 21:52:28 +0100

    fix(tests): add kill tests module

---
+++ tests/kills.test.js
@@ -0,0 +1,38 @@
+import { describe, it, expect } from 'vitest';
+import { process_champion_kill } from '../data/kills.js';
+
+describe('process_champion_kill', () => {
+    const event = {
+        killerId: 6,
+        victimId: 5,
+        assistingParticipantIds: [2, 1, 8, 7]
+    };
+
+    const state = {
+        teams: [
+            {
+                players: [
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                ]
+            },{
+                players: [
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                    {kills: 0, deaths: 0, assists: 0},
+                ]
+            },
+        ]
+    };
+
+    process_champion_kill(state, event);
+
+    it('updates the killers kda', () => {
+        expect(state.teams[1].players[0].kills).toBe(1);
+    });
+});
