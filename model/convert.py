df0530770ade4ce9b229882770fc79e9e7867ccf
Fahd <fahd31256@gmail.com>
2025-08-07 21:34:00 +0100

    feat(model): add script to convert model to onnx format

---
+++ model/convert.py
@@ -0,0 +1,17 @@
+import torch
+from evaluate import load_network, load_dataset
+
+network = load_network()
+dataset = load_dataset()
+
+dummy_data, _ = next(iter(dataset))
+
+dummy_input = torch.zeros(size=(1, len(dummy_data)), dtype=torch.int32)
+torch.onnx.export(
+    network,
+    dummy_input,
+    "model.onnx",
+    input_names=["input"],
+    output_names=["output"],
+    opset_version=16,
+)
