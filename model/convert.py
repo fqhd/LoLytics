e9ccc87be2af72223ac776648330822a4cd12b80
Fahd <fahd31256@gmail.com>
2025-08-09 12:46:36 +0100

    feat: print number of inputs during onnx conversion

--- model/convert.py
+++ model/convert.py
@@ -6,6 +6,8 @@ dataset = load_dataset()
 
 dummy_data, _ = next(iter(dataset))
 
+print('Number of Inputs:', len(dummy_data))
+
 dummy_input = torch.zeros(size=(1, len(dummy_data)), dtype=torch.int32)
 torch.onnx.export(
     network,
