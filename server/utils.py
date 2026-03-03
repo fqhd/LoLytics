f74f355880eb3dce5fcb06cfd84a182fa5fcbc7e
Fahd <fahd31256@gmail.com>
2026-03-03 14:00:38 +0000

    refactor(utils.py): use single quotes

--- server/utils.py
+++ server/utils.py
@@ -1,6 +1,6 @@
 def find_participant_with_puuid(participants, puuid):
     for p in participants:
-        if p.get("puuid") == puuid:
+        if p.get('puuid') == puuid:
             return p
     return None
 
@@ -9,12 +9,12 @@ def find_opponent(participants, player):
     if not player:
         return None
 
-    position = player.get("teamPosition")
+    position = player.get('teamPosition')
 
     for p in participants:
         if (
-            p.get("teamPosition") == position
-            and p.get("puuid") != player.get("puuid")
+            p.get('teamPosition') == position
+            and p.get('puuid') != player.get('puuid')
         ):
             return p
 
